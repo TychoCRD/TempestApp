74cb19af5a3b84d47010968f7898438c79dd0877
TychoCRD <rtcotton@gmail.com>
2016-09-10 23:16:30 +0800

    Add tests for Actions and Reducers, update packages

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,74 @@
+import expect from 'expect';
+
+import * as actions from 'actions';
+
+describe('Actions', ()=>{
+
+  it('should generate update appStage action', ()=>{
+    var action = {
+      type: 'UPDATE_APP_STAGE',
+      nextStage: 'test'
+    };
+    var res = actions.updateAppStage(action.nextStage);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate increaseCount action', ()=>{
+    var action = {
+      type: 'INCREASE_COUNT'
+    };
+    var res = actions.increaseCount();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate decreaseCount action', ()=>{
+    var action = {
+      type: 'DECREASE_COUNT'
+    };
+    var res = actions.decreaseCount();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate startCountdown action', ()=>{
+    var action = {
+      type: 'START_COUNTDOWN'
+    };
+    var res = actions.startCountdown();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate stopCountdown action', ()=>{
+    var action = {
+      type: 'STOP_COUNTDOWN'
+    };
+    var res = actions.stopCountdown();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate update addKeyword action', ()=>{
+    var action = {
+      type: 'ADD_KEYWORD',
+      text: 'test'
+    };
+    var res = actions.addKeyword(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate removeKeyword action', ()=>{
+    var action = {
+      type: 'REMOVE_KEYWORD',
+      text: 'test'
+    };
+    var res = actions.removeKeyword(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+
+});
---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,85 @@
+import expect from 'expect';
+import df from 'deep-freeze-strict';
+
+import * as reducers from 'reducers';
+
+describe('Reducers', ()=>{
+
+  describe('appStageReducer', ()=>{
+    it('should update app stage', ()=>{
+      var action = {
+        type: 'UPDATE_APP_STAGE',
+        nextStage: 'test'
+      };
+      var res = reducers.appStageReducer(df('settings'), df(action));
+
+      expect(res).toEqual(action.nextStage);
+    });
+
+  });
+
+  describe('countReducer', ()=>{
+    it('should increase count', ()=>{
+      var action = {
+        type: 'INCREASE_COUNT'
+      };
+      var res = reducers.countReducer(df(100), df(action));
+
+      expect(res).toEqual(130);
+    });
+
+    it('should ecrease count', ()=>{
+      var action = {
+        type: 'DECREASE_COUNT'
+      };
+      var res = reducers.countReducer(df(100), df(action));
+
+      expect(res).toEqual(70);
+    });
+
+  });
+
+  describe('countdownStatusReducer', ()=>{
+    it('should start countdownStatus', ()=>{
+      var action = {
+        type: 'START_COUNTDOWN'
+      };
+      var res = reducers.countdownStatusReducer(df('stopped'), df(action));
+
+      expect(res).toEqual('started');
+    });
+
+    it('should stop countdownStatus', ()=>{
+      var action = {
+        type: 'STOP_COUNTDOWN'
+      };
+      var res = reducers.countdownStatusReducer(df('started'), df(action));
+
+      expect(res).toEqual('stopped');
+    });
+
+  });
+
+  describe('masterKeywordsReducer', ()=>{
+    it('should add new keyword', ()=>{
+      var action = {
+        type: 'ADD_KEYWORD',
+        text: 'test'
+      };
+      var res = reducers.masterKeywordsReducer(df([]), df(action));
+
+      expect(res).toEqual(['test']);
+    });
+
+    it('should remove keyword', ()=>{
+      var action = {
+        type: 'REMOVE_KEYWORD',
+        text: 'test'
+      };
+      var res = reducers.masterKeywordsReducer(df(['test']), df(action));
+
+      expect(res).toEqual([]);
+    });
+  });
+
+}); // End of Reducers describe
